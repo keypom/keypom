9d983c42d7020d14451f2f240ed2d8da7cb908b8
Willem Wyndham <willem@cs.umd.edu>
2021-07-30 17:20:49 -0400

    fix: add Cargo.toml to top level to simplify build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 mkdir -p ../out
-cp target/wasm32-unknown-unknown/release/*.wasm ../out/main.wasm
+cp target/wasm32-unknown-unknown/release/*.wasm ./out/main.wasm
