b508e09410069a2b314b6a8a5a78f5df47a2f73a
BenKurrek <bkurrek@uwaterloo.ca>
2022-05-26 09:55:40 -0400

    fixed build script and updated gitignore

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-mkdir -p ../out
+mkdir -p ./out
 cp target/wasm32-unknown-unknown/release/*.wasm ./out/main.wasm
