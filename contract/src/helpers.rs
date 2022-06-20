e7c752dd003cc4cd0a42410643641c95133f2e1e
Benjamin Kurrek <57506486+BenKurrek@users.noreply.github.com>
2022-06-20 09:29:11 -0400

    Update contract/src/helpers.rs
    Co-authored-by: Austin Abell <austinabell8@gmail.com>

--- contract/src/helpers.rs
+++ contract/src/helpers.rs
@@ -2,11 +2,7 @@ use crate::*;
 
 //used to generate a unique prefix in our storage collections (this is to avoid data collisions)
 pub(crate) fn hash_account_id(account_id: &AccountId) -> CryptoHash {
-    //get the default hash
-    let mut hash = CryptoHash::default();
-    //we hash the account ID and return it
-    hash.copy_from_slice(&env::sha256(account_id.as_bytes()));
-    hash
+    env::sha256_array(account_id.as_bytes())
 }
 
 impl LinkDropProxy {
