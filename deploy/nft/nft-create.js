6ffc132db33ddebf75e7c8c4d9f13c5e7456733c
BenKurrek <bkurrek@uwaterloo.ca>
2022-09-29 10:32:04 -0400

    nft demo finished

--- deploy/nft/nft-create.js
+++ deploy/nft/nft-create.js
@@ -4,9 +4,11 @@ const path = require("path");
 const homedir = require("os").homedir();
 const { writeFile, mkdir, readFile } = require('fs/promises');
   
-let LINKDROP_PROXY_CONTRACT_ID = process.env.CONTRACT_NAME;
+let KEYPOM_CONTRACT = process.env.CONTRACT_NAME;
 let FUNDING_ACCOUNT_ID = process.env.FUNDING_ACCOUNT_ID;
 let LINKDROP_NEAR_AMOUNT = process.env.LINKDROP_NEAR_AMOUNT;
+let NFT_CONTRACT_ID = process.env.NFT_CONTRACT_ID;
+let TOKEN_ID = process.env.TOKEN_ID;
 
 let OFFSET = 1;
 let DROP_FEE = 1;
@@ -16,25 +18,6 @@ let NUM_KEYS = 1;
 let NETWORK_ID = "testnet";
 let near;
 let keyStore;
-
-/*
-	Hard coding NFT contract and metadata. Change this if you want.
-*/
-let NFT_CONTRACT_ID = "nft.examples.testnet";
-const METADATA = {
-	"title": "Linkdropped Go Team NFT",
-	"description": "Testing Linkdrop NFT Go Team Token",
-	"media": "https://bafybeiftczwrtyr3k7a2k4vutd3amkwsmaqyhrdzlhvpt33dyjivufqusq.ipfs.dweb.link/goteam-gif.gif",
-	"media_hash": null,
-	"copies": 10000,
-	"issued_at": null,
-	"expires_at": null,
-	"starts_at": null,
-	"updated_at": null,
-	"extra": null,
-	"reference": null,
-	"reference_hash": null
-};
 
 // set up near
 const initiateNear = async () => {
@@ -60,30 +43,30 @@ async function start() {
 	//deployed linkdrop proxy contract
 	await initiateNear();
 
-	if(!LINKDROP_PROXY_CONTRACT_ID) {
+	if(!KEYPOM_CONTRACT) {
 		const dev_account = await readFile(`neardev/dev-account`);
-		LINKDROP_PROXY_CONTRACT_ID = dev_account.toString();
+		KEYPOM_CONTRACT = dev_account.toString();
 	}
 
-	console.log('LINKDROP_PROXY_CONTRACT_ID: ', LINKDROP_PROXY_CONTRACT_ID);
+	console.log('KEYPOM_CONTRACT: ', KEYPOM_CONTRACT);
 	console.log('FUNDING_ACCOUNT_ID: ', FUNDING_ACCOUNT_ID);
 	console.log('LINKDROP_NEAR_AMOUNT: ', LINKDROP_NEAR_AMOUNT);
 
-	if(!FUNDING_ACCOUNT_ID || !LINKDROP_NEAR_AMOUNT) {
-		throw "must specify funding account and linkdrop near amount";
+	if(!FUNDING_ACCOUNT_ID || !LINKDROP_NEAR_AMOUNT || !NFT_CONTRACT_ID) {
+		throw "must specify funding account and linkdrop near amount and nft contract ID";
 	}
 
-	const contractAccount = await near.account(LINKDROP_PROXY_CONTRACT_ID);
+	const contractAccount = await near.account(KEYPOM_CONTRACT);
 	const fundingAccount = await near.account(FUNDING_ACCOUNT_ID);
 
-	console.log(`initializing contract for account ${LINKDROP_PROXY_CONTRACT_ID}`);
+	console.log(`initializing contract for account ${KEYPOM_CONTRACT}`);
 	try {
 		await contractAccount.functionCall(
-			LINKDROP_PROXY_CONTRACT_ID, 
+			KEYPOM_CONTRACT, 
 			'new', 
 			{
 				root_account: "testnet",
-				owner_id: LINKDROP_PROXY_CONTRACT_ID
+				owner_id: KEYPOM_CONTRACT
 			}, 
 			"300000000000000", 
 		);
@@ -105,18 +88,18 @@ async function start() {
 	console.log("Finished.");
 
 	const dropId = await fundingAccount.viewFunction(
-		LINKDROP_PROXY_CONTRACT_ID, 
+		KEYPOM_CONTRACT, 
 		'get_next_drop_id',
 	);
 
 	try {
 		await fundingAccount.functionCall(
-			LINKDROP_PROXY_CONTRACT_ID, 
+			KEYPOM_CONTRACT, 
 			'add_to_balance', 
 			{},
 			"300000000000000", 
 			parseNearAmount(
-				((parseFloat(LINKDROP_NEAR_AMOUNT) + KEY_FEE + OFFSET) * pubKeys.length * config.uses_per_key || 1 + DROP_FEE).toString()
+				((parseFloat(LINKDROP_NEAR_AMOUNT) + KEY_FEE + OFFSET) * pubKeys.length * 1 + DROP_FEE).toString()
 			)
 		);
 	} catch(e) {
@@ -127,17 +110,14 @@ async function start() {
 		let nft_data = {};
 		nft_data["contract_id"] = NFT_CONTRACT_ID;
 		nft_data["sender_id"] = FUNDING_ACCOUNT_ID;
-		nft_data["longest_token_id"] = pubKeys[0];
 
 		await fundingAccount.functionCall(
-			LINKDROP_PROXY_CONTRACT_ID, 
+			KEYPOM_CONTRACT, 
 			'create_drop', 
 			{
 				public_keys: pubKeys,
 				deposit_per_use: parseNearAmount(LINKDROP_NEAR_AMOUNT),
-				nft_data,
-				config,
-				metadata: JSON.stringify(metadata)
+				nft_data
 			}, 
 			"300000000000000"
 		);
@@ -146,40 +126,13 @@ async function start() {
 	}
 
 	try {
-		const tokens = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_nft_token_ids_for_drop',
-			{
-				drop_id: dropId
-			}
-		);
-		viewData.token_ids_for_drop_before = tokens; 
-		console.log('tokens: ', tokens);
-	} catch(e) {
-		console.log("Error getting tokens: ", e);
-	}
-
-	try {
-		console.log(`minting NFT with token ID ${pubKeys[0]} on contract ${NFT_CONTRACT_ID} with receiver_id: ${FUNDING_ACCOUNT_ID}`);
-		await fundingAccount.functionCall(
-			NFT_CONTRACT_ID, 
-			'nft_mint', 
-			{
-				token_id: pubKeys[0],
-				receiver_id: FUNDING_ACCOUNT_ID,
-				metadata: METADATA,
-			}, 
-			"300000000000000", 
-			parseNearAmount('1')
-		);
-
 		console.log(`transferring NFT to linkdrop proxy contract with nft_transfer_call`);
 		await fundingAccount.functionCall(
 			NFT_CONTRACT_ID, 
 			'nft_transfer_call', 
 			{
-				token_id: pubKeys[0],
-				receiver_id: LINKDROP_PROXY_CONTRACT_ID,
+				token_id: TOKEN_ID,
+				receiver_id: KEYPOM_CONTRACT,
 				msg: dropId.toString(),
 			}, 
 			"300000000000000", 
@@ -189,90 +142,10 @@ async function start() {
 		console.log('error sending FTs: ', e);
 	}
 
-	try {
-		const totalSupply = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_key_total_supply', 
-		);
-		viewData.get_key_total_supply = totalSupply; 
-		console.log('totalSupply: ', totalSupply);
-
-		const getKeys = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_keys'
-		);
-		viewData.get_keys = getKeys; 
-		console.log('getKeys: ', getKeys);
-
-		const keyInfo = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_key_information',
-			{
-				key: pubKeys[0]
-			}
-		);
-		viewData.get_key_information = keyInfo; 
-		console.log('keyInfo: ', keyInfo);
-
-		const dropInfo = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_drop_information',
-			{
-				drop_id: dropId
-			}
-		);
-		viewData.get_drop_information = dropInfo; 
-		console.log('dropInfo: ', dropInfo);
-
-		const keysForDrop = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_keys_for_drop',
-			{
-				drop_id: dropId
-			}
-		);
-		viewData.get_keys_for_drop = keysForDrop; 
-		console.log('keysForDrop: ', keysForDrop);
-
-		const dropSupplyForFunder = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_drop_supply_for_owner',
-			{
-				account_id: FUNDING_ACCOUNT_ID
-			}
-		);
-		viewData.get_drop_supply_for_owner = dropSupplyForFunder; 
-		console.log('dropSupplyForFunder: ', dropSupplyForFunder);
-
-		const dropsForFunder = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_drops_for_owner',
-			{
-				account_id: FUNDING_ACCOUNT_ID
-			}
-		);
-		viewData.get_drops_for_owner = dropsForFunder; 
-		console.log('dropsForFunder: ', dropsForFunder);
-
-		const tokens = await fundingAccount.viewFunction(
-			LINKDROP_PROXY_CONTRACT_ID, 
-			'get_nft_token_ids_for_drop',
-			{
-				drop_id: dropId
-			}
-		);
-		viewData.token_ids_for_drop_after = tokens; 
-		console.log('tokens after: ', tokens);
-
-		await writeFile(path.resolve(__dirname, `views-create.json`), JSON.stringify(viewData));
-	} catch(e) {
-		console.log('error initializing contract: ', e);
-	}
-	
 	let curPks = {};
 	for(var i = 0; i < keyPairs.length; i++) {
-		curPks[keyPairs[i].publicKey.toString()] = `https://wallet.testnet.near.org/linkdrop/${LINKDROP_PROXY_CONTRACT_ID}/${keyPairs[i].secretKey}`;
-		console.log(`https://wallet.testnet.near.org/linkdrop/${LINKDROP_PROXY_CONTRACT_ID}/${keyPairs[i].secretKey}`);
+		curPks[keyPairs[i].publicKey.toString()] = `https://testnet.mynearwallet.com/linkdrop/${KEYPOM_CONTRACT}/${keyPairs[i].secretKey}`;
+		console.log(`https://testnet.mynearwallet.com/linkdrop/${KEYPOM_CONTRACT}/${keyPairs[i].secretKey}`);
 		console.log("Pub Key: ", keyPairs[i].publicKey.toString());
 	}
 
